f98aa5005ffe1254495e45655d5c97cefb4456cf
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-17 16:24:10 +0200

    Update colocar_iconos.sh

--- colocar_iconos.sh
+++ colocar_iconos.sh
@@ -16,12 +16,8 @@
 cd /home/pi/Desktop
 sudo rm *.desktop
 # recupera los iconos que se han borrado y se han convertido en icono de un libro 
-# pero no quita los semaforos que se queden encendidos por error al reiniciar
 cp /home/pi/SYSTEM/Desktop/*.desktop /home/pi/Desktop
 sudo chmod 777 -R /home/pi/Desktop
-
-#sudo chmod 777 -R /home/pi/SYSTEM
-
 
 sudo cp /home/pi/icons.screen0-1904x1017.rc /home/pi/.config/xfce4/desktop
 sudo chmod 777 -R /home/pi/.config/xfce4/desktop
