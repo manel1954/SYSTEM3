eded1007a42ec19e91bd93ae0efdb4b56f7b418f
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-17 16:19:36 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -3,16 +3,13 @@
                                           
                         cd /home/pi/SYSTEM3
                         
-                        git pull --force
-
-                        
+                        git pull --force                      
 
                         sudo rm -R /home/pi/SYSTEM
 
                         mkdir /home/pi/SYSTEM
                                                 
-                        cp -R /home/pi/SYSTEM3/* /home/pi/SYSTEM
-                        #cp -R /home/pi/SYSTEM3/AUTOARRANQUE_SYSTEM/* /home/pi/AUTOARRANQUE_SYSTEM                      
+                        cp -R /home/pi/SYSTEM3/* /home/pi/SYSTEM                     
                         
                         sudo rm -R /home/pi/SYSTEM/qt
 
@@ -23,7 +20,8 @@
                         
                         sudo chmod 777 -R /home/pi/SYSTEM/qt
 
-                        #sudo chmod 777 -R /home/pi/AUTOARRANQUE_SYSTEM
+                        cd /home/pi/SYSTEM/qt/
+                        ./qt_actualizando_imagen
 
                         
 
