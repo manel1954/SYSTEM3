1fbff16b5fdda6bee2fe245cbe5be202cea7cafd
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-17 16:13:38 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -12,18 +12,18 @@
                         mkdir /home/pi/SYSTEM
                                                 
                         cp -R /home/pi/SYSTEM3/* /home/pi/SYSTEM
-                        cp -R /home/orangepi/SYSTEM3/AUTOARRANQUE_SYSTEM/* /home/orangepi/AUTOARRANQUE_SYSTEM                      
+                        #cp -R /home/pi/SYSTEM3/AUTOARRANQUE_SYSTEM/* /home/pi/AUTOARRANQUE_SYSTEM                      
                         
-                        sudo rm -R /home/pi/qt
+                        sudo rm -R /home/pi/SYSTEM/qt
 
                                       
-                        cp -R /home/pi/SYSTEM3/qt/ /home/pi/
-                        
+                        cp -R /home/pi/SYSTEM3/qt/ /home/pi/SYSTEM/
+
                         sudo chmod 777 -R /home/pi/SYSTEM
                         
-                        sudo chmod 777 -R /home/pi/qt
+                        sudo chmod 777 -R /home/pi/SYSTEM/qt
 
-                        sudo chmod 777 -R /home/pi/AUTOARRANQUE_SYSTEM
+                        #sudo chmod 777 -R /home/pi/AUTOARRANQUE_SYSTEM
 
                         
 
