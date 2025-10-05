e353a1642282f7c249c1854ea6f625b196e88a5f
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-10-06 01:14:15 +0200

    Update actualizar_REFLECTORES_YSF.sh

--- actualizar_REFLECTORES_YSF.sh
+++ actualizar_REFLECTORES_YSF.sh
@@ -8,13 +8,34 @@ CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
 
-			cd /home/pi/YSFClients/YSFGateway
-			sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php
-            clear
-			echo "${VERDE}*********************************"
-					echo "* ACTUALIZANDO REFLECTORES YSF  *"
-					echo "*********************************"
+			#cd /home/pi/YSFClients/YSFGateway
+			#sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php
+            #clear
+			#echo "${VERDE}*********************************"
+			#		echo "* ACTUALIZANDO REFLECTORES YSF  *"
+			#		echo "*********************************"
+			#sleep 3
+
+
+
+			
+			
+			
+			
+			# cd /home/pi/YSFClients/YSFGateway
+			#sudo wget -O YSFHosts.txt http://register.ysfreflector.de/export_csv.php 
+            
+			
+			#curl https://dvref.com/downloads/YSFHosts-resolved.txt > /home/pi/YSFClients/YSFGateway/YSFHosts.txt
+			
+			cp /home/pi/SYSTEM/YSFHosts.txt /home/pi/YSFClients/YSFGateway/
+
+
 			sleep 3
-
-
-
+			
+			clear
+			echo "${VERDE}**********************************"
+					echo "*  ACTUALIZANDO REFLECTORES YSF  *"
+					echo "**********************************"
+			sleep 3
+			
