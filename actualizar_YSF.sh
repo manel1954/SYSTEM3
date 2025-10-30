fd13f9ce34982624bd3c8b3efe69afe485a60e45
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-10-30 20:21:35 +0100

    Update actualizar_YSF.sh

--- actualizar_YSF.sh
+++ actualizar_YSF.sh
@@ -16,7 +16,7 @@ cp /home/pi/SYSTEM/YSFHosts.txt /home/pi/YSFClients/YSFGateway/
 			
 			clear
 			echo "${VERDE}**********************************"
-					echo "*  ACTUALIZANDO REFLECTORES YSF CCCCC XX*"
+					echo "*  ACTUALIZANDO REFLECTORES YSF  *"
 					echo "**********************************"
 			sleep 3
 						
